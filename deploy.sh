e734afd8bbc17c15588099097dc3183704f64b69
Paulo Lopes <paulo.lopes@reserve.com.br>
2021-02-25 12:19:56 -0300

    Adicionado auto continue na instalação     Adicionado bloco de deploy do app     Adicionado instalação do pip

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-#bloco de instalação de requisitos para o abiente
+#bloco de instalação de requisitos para o ambiente
 apt update
-apt install python3
-apt install git
+apt install python3 -y
+apt install python3-pip -y
+apt install git -y
 apt update
+
+#Criador diretorio e feito download do app
+mkdir app
+cd app
+git clone https://github.com/gohackfelipe/desafio.git
+cd desafio
+cd app
--- history.md
+++ history.md
@@ -1,6 +1,10 @@
+### v3
+    Adicionado auto continue na instalação
+    Adicionado bloco de deploy do app
+    Adicionado instalação do pip
+
 ### v2
-Alterado script para preparação do ambiente de deploy
+    Alterado script para preparação do ambiente de deploy
 
 ### v1
-
-Criação do script para testar sua execução
+    Criação do script para testar sua execução
